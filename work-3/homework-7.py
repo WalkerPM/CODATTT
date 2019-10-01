014f5b352cef0dfb6378b52418956ac8b4e59da8
Ivakin Bogdan <minefirst007@gmail.com>
2019-10-01 18:16:09 +0500

    WIP

--- work-3/homework-7.py
+++ work-3/homework-7.py
@@ -1,4 +1,15 @@
-pair = 20
+#!/usr/bin/python3
+pair = 10
 numbers = []
-for i in range(pair)
-	num = 
+out = []
+for i in range(pair):
+	num = int(input("Num1: "))
+	num2 = int(input("Num2: "))
+	numbers.append(num)
+	numbers.append(num2)
+print(numbers)
+
+
+for i in range(0, len(numbers), 2):
+	out.append(max(numbers[i],numbers[i + 1]))
+print(out)
